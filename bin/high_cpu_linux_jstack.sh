b7e48129b76f2f43fbc94a16fdf515a4dd72b37f
Yunfeng Zhao <yuzhao@redhat.com>
2017-04-18 09:23:19 +0800

    Revert "Forcing parameters on the shell script to capture OS data"
    This reverts commit 2de925174bb240a8804dfbc07d36f34c61326902.

--- bin/high_cpu_linux_jstack.sh
+++ bin/high_cpu_linux_jstack.sh
@@ -6,34 +6,27 @@
 # times and INTERVAL. Thread dumps are retrieved using jstack and are placed in 
 # high-cpu-tdump.out
 #
-# Usage: sh ./high_cpu_linux_jstack.sh <JBOSS_PID> <LOOP> <INTERNVAL>
+# Usage: sh ./high_cpu_linux_jstack.sh <JBOSS_PID>
 #
 # Change Log:
 # * 2011-05-02 19:00 <loleary>
 # - Added date output to high-cpu.out for log correlation
 # - Added -p argument to top to limit high-cpu.out to <JBOSS_PID>
-# - Enfored usage of parameters
+#
 #
 
-if [[ $# -ne 3 ]]; then
-  echo "Usage: `basename $0` <pid> <num_of_samples> <interval>"
-  exit 1
-fi
-
 # Number of times to collect data.
-LOOP=$2
+LOOP=6
 # Interval in seconds between data points.
-INTERVAL=$3
-# PID to be captured
-PID=$1
+INTERVAL=20
 
 for ((i=1; i <= $LOOP; i++))
 do
    _now=$(date)
    echo "${_now}" >>high-cpu.out
-   top -b -n 1 -H -p $PID >>high-cpu.out
+   top -b -n 1 -H -p $1 >>high-cpu.out
    echo "${_now}" >>high-cpu-tdump.out
-   jstack -l $PID >>high-cpu-tdump.out
+   jstack -l $1 >>high-cpu-tdump.out
    echo "thread dump #" $i
    if [ $i -lt $LOOP ]; then
       echo "Sleeping..."
